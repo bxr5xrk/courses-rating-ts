b022b95ab208cd6162e85ecaf2ed1506a0743d79
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-07-13 17:14:01 +0300

    added footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { FooterProps } from "./Footer.props";
+import st from "./Footer.module.css";
+import format from "date-fns/format";
+
 
 const Footer = ({ ...props }: FooterProps) => {
     return (
         <footer {...props}>
-            <p>Top Courses</p>
-            <p>sdcsdcsdcd</p>
+            <div className={st.footer}>
+                <p>Top Courses © 2021 - {format(new Date(), 'yyyy')} All rights reserved</p>
+                <div>
+                    <a href="/">Terms of use</a>
+                    <a href="/">Privacy Policy</a>
+                </div>
+            </div>
         </footer>
     );
 };
