a6668c9af119d1edc036c24d8870bc4e749de0e8
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-07-13 16:40:33 +0300

    added grid

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,17 +2,18 @@ import React, { ReactNode } from "react";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Sidebar from "./Sidebar/Sidebar";
+import st from "./Layout.module.css";
 
 const Layout = ({ children }: { children: ReactNode }) => {
     return (
-        <>
-            <Header />
-            <section>
-                <Sidebar />
-                <main>{children}</main>
-            </section>
-            <Footer />
-        </>
+        <div className={st.wrapper}>
+            <Header className={st.header} />
+
+            <Sidebar className={st.sidebar} />
+            <div className={st.main}>{children}</div>
+            <Footer className={st.footer} />
+
+        </div>
     );
 };
 
